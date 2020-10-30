130722453235d22d4df03b5bf8d63481eb107df9
Alexandre Herve <alexandre.herve@dashlane.com>
2020-10-30 20:34:22 +0100

    remove dev user module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,15 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypegooseModule } from 'nestjs-typegoose';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypegooseModule.forRoot('mongodb://admin:password@mongodb:27017/oclize', {
       useUnifiedTopology: true,
       useNewUrlParser: true
-    }),
-    UserModule
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
--- server/src/user/domain/user.ts
+++
@@ -1,6 +0,0 @@
-import { prop } from '@typegoose/typegoose';
-
-export class User {
-    @prop()
-    name?: string;
-}
