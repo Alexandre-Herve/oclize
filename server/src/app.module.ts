f2c7e72c02132fa940d68523d5d2d1c4a658e5b3
Alexandre Herve <alexandre.herve@dashlane.com>
2020-10-31 12:01:10 +0100

    setup typegoose from config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,20 +3,33 @@ import { AppService } from './app.service';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const typegooseFactory = async (configService: ConfigService) => {
+  const port = configService.get<string>('MONGODB_PORT', '27017')
+  const host = configService.get<string>('MONGODB_HOST', 'localhost')
+  const username = configService.get<string>('MONGODB_USERNAME', 'admin')
+  const password = configService.get<string>('MONGODB_PASSWORD', 'password')
+  const dbname = configService.get<string>('MONGODB_DATABASE', 'oclize')
+  const uri = `mongodb://${username}:${password}@${host}:${port}/${dbname}`
+  return {
+    uri,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+}
 
 @Module({
   imports: [
-    TypegooseModule.forRoot('mongodb://admin:password@mongodb:27017/oclize', {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: typegooseFactory,
+      inject: [ConfigService]
     }),
     AuthModule,
     UsersModule,
-    ConfigModule.forRoot({
-      isGlobal: true
-    })
+    ConfigModule.forRoot({ isGlobal: true })
   ],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
